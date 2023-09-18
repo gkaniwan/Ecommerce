e97ae6b9c5197309dee0e714868edccd7dae5150
gcaniguv <gonzalo@kaniwan.cl>
2023-09-17 23:15:03 -0300

    agrega filtro por categoria

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import  Header  from './src/components/Header';
 import Categories from './src/components/Categories';
+import Search from './src/components/Search';
+// import Home from './src/screens/Home';
 
 
 export default function App() {
   return (
       <View >
+      {/* <Home/> */}
+        {/* <Search/> */}
         <Header />
         <Categories/>
       </View>
